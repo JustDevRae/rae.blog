f46151fa3d4e9d766d4e80a006491bb9a8e30881
JustDevRae <ksrae165@gmail.com>
2025-02-22 22:38:38 +0900

    remove: TOC 컴포넌트 삭제

--- src/app/post/[slug]/page.tsx
+++ src/app/post/[slug]/page.tsx
@@ -1,7 +1,6 @@
-import { getMdxFileDataBySlug, getHeadingsForTOC } from "@/utils/parseMdx";
+import { getMdxFileDataBySlug } from "@/utils/parseMdx";
 import { MDXRemote } from "next-mdx-remote/rsc";
 import CustomComponents from "@/components/mdx/CustomComponents";
-import TableOfContent from "@/components/TableOfContent";
 
 export async function generateStaticParams() {
   const { getAllMdxMetadataAndSlug } = await import("@/utils/parseMdx");
@@ -19,13 +18,9 @@ export default async function PostPage({
 }) {
   const { slug } = await params;
   const { mdxContent, mdxMetaData } = getMdxFileDataBySlug(slug);
-  const headings = getHeadingsForTOC(slug);
 
   return (
     <div>
-      <aside className="w-full lg:w-1/4">
-        <TableOfContent headings={headings} />
-      </aside>
       <article className="prose mx-auto">
         <h1>{mdxMetaData.title}</h1>
         <p>{mdxMetaData.date}</p>
