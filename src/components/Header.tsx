0fadf1d0f75d380b2631db14c80ce40b0a8020e3
JustDevRae <ksrae165@gmail.com>
2025-02-24 15:33:53 +0900

    chore: prettier-plugin-tailwindcss 버전을 0.6.8에서 0.6.11로 업데이트

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Header() {
   return (
-    <header className="mobile:justify-between fixed left-1/2 top-0 flex h-[50px] w-full max-w-[1200px] -translate-x-1/2 transform items-center justify-around bg-gray-800 px-[30px] font-semibold text-white">
+    <header className="fixed left-1/2 top-0 flex h-[50px] w-full max-w-[1200px] -translate-x-1/2 transform items-center justify-around bg-gray-800 px-[30px] font-semibold text-white mobile:justify-between">
       <div>
         <Link href="/" className="hover:text-cyan-400">
           Rae.
