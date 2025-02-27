aa323414e4030c732b393b81818db3ad69b2917b
JustDevRae <ksrae165@gmail.com>
2025-02-27 09:26:07 +0900

    feat: 스크롤 시 헤더 숨김 기능을 위한 커스텀 훅 구현

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,19 @@
+"use client";
+
+import { useShowAndHideHeader } from "@/hooks/useShowAndHideHeader";
 import Link from "next/link";
 import ThemeDropdown from "./ThemeDropdown";
 import Logo from "./Logo";
 
 export default function Header() {
+  const { visible } = useShowAndHideHeader();
+
   return (
-    <header className="fixed left-1/2 top-0 flex h-[50px] w-full max-w-[1200px] -translate-x-1/2 transform items-center justify-around px-[30px] font-semibold mobile:justify-between">
+    <header
+      className={`fixed left-1/2 top-0 flex h-[50px] w-full max-w-[1200px] -translate-x-1/2 transform items-center justify-around px-[30px] font-semibold transition-transform duration-300 mobile:justify-between ${
+        visible ? "translate-y-0" : "-translate-y-full"
+      }`}
+    >
       <div>
         <Logo />
       </div>
