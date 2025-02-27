3b3268b1834761ee80a0e887adf728d7489fec83
JustDevRae <ksrae165@gmail.com>
2025-02-27 21:53:21 +0900

    feat: 이스터에그 페이지 틱택토 게임 추가

---
+++ src/app/easter-egg/[tic-tac-toe]/page.tsx
@@ -0,0 +1,90 @@
+"use client";
+
+import { useState } from "react";
+
+interface SquareProps {
+  value: string | null;
+  onSquareClick: () => void;
+}
+
+function Square({ value, onSquareClick }: SquareProps) {
+  return (
+    <button
+      type="button"
+      onClick={onSquareClick}
+      className="float-left -mr-px -mt-px h-[50px] w-[50px] border border-gray-600 bg-white p-0 text-center text-2xl font-bold leading-[34px] text-black dark:bg-black dark:text-white"
+    >
+      {value}
+    </button>
+  );
+}
+
+function calculateWinner(squares: (string | null)[]) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  for (let i = 0; i < lines.length; i += 1) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
+
+export default function Board() {
+  const [xIsNext, setXIsNext] = useState(true);
+  const [squares, setSquares] = useState<(string | null)[]>(
+    Array(9).fill(null),
+  );
+
+  const winner = calculateWinner(squares);
+  let status;
+  if (winner) {
+    status = `Winner: ${winner}`;
+  } else {
+    status = `Next player: ${xIsNext ? "X" : "O"}`;
+  }
+
+  const handleClick = (index: number) => {
+    if (squares[index] || calculateWinner(squares)) {
+      return;
+    }
+    const nextSquares = squares.slice();
+    if (xIsNext) {
+      nextSquares[index] = "X";
+    } else {
+      nextSquares[index] = "O";
+    }
+    setSquares(nextSquares);
+    setXIsNext(!xIsNext);
+  };
+  return (
+    <>
+      <div className="mb-3 text-xl font-bold">{status}</div>
+      <div>
+        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
+        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
+      </div>
+      <div>
+        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
+        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
+        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
+      </div>
+      <div>
+        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
+        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
+        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
+      </div>
+    </>
+  );
+}
--- src/app/easter-egg/page.tsx
+++ src/app/easter-egg/page.tsx
@@ -1,3 +1,12 @@
-export default async function Home() {
-  return <main> Hello! This is Easter Egg Page!</main>;
+import Link from "next/link";
+
+export default function EasterEgg() {
+  return (
+    <main className="flex flex-col items-center justify-center">
+      <p>Hello! This is Easter Egg Page!</p>
+      <div>
+        <Link href="/easter-egg/tic-tac-toe">Tic-Tac-Toe</Link>
+      </div>
+    </main>
+  );
 }
