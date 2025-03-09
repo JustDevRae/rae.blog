0626b1ab95024e2c439fed70eb058d726935d445
JustDevRae <ksrae165@gmail.com>
2025-03-10 08:36:21 +0900

    design: post head 디자인 설정

--- src/app/post/[slug]/page.tsx
+++ src/app/post/[slug]/page.tsx
@@ -21,13 +21,21 @@ export default async function PostDetailPage({
   const { mdxContent, mdxMetaData } = getMdxFileDataBySlug(slug);
 
   return (
-    <div>
-      <article className="prose prose-sm dark:prose-invert">
-        <h2>{mdxMetaData.title}</h2>
-        <p>{mdxMetaData.date}</p>
+    <article className="prose prose-sm mt-[50px] dark:prose-invert">
+      <section className="mb-6 flex flex-col items-center border-b pb-4">
+        <h2 className="text-2xl font-bold tracking-tight">
+          {mdxMetaData.title}
+        </h2>
+        <p className="mt-1 text-sm text-gray-500">{mdxMetaData.date}</p>
+      </section>
+
+      <section>
         <MDXRemote source={mdxContent} components={CustomComponents} />
+      </section>
+
+      <section>
         <Comments />
-      </article>
-    </div>
+      </section>
+    </article>
   );
 }
