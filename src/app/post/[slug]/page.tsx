29d31eff05dd8fa587f95ec634783c653cb34ff8
JustDevRae <ksrae165@gmail.com>
2025-03-10 13:58:49 +0900

    chore: post head 의 제목 태그 변경

--- src/app/post/[slug]/page.tsx
+++ src/app/post/[slug]/page.tsx
@@ -23,19 +23,14 @@ export default async function PostDetailPage({
   return (
     <article className="prose prose-sm mt-[50px] dark:prose-invert">
       <section className="mb-6 flex flex-col items-center border-b pb-4">
-        <h2 className="text-2xl font-bold tracking-tight">
+        <h1 className="text-2xl font-bold tracking-tight">
           {mdxMetaData.title}
-        </h2>
+        </h1>
         <p className="mt-1 text-sm text-gray-500">{mdxMetaData.date}</p>
       </section>
 
-      <section>
-        <MDXRemote source={mdxContent} components={CustomComponents} />
-      </section>
-
-      <section>
-        <Comments />
-      </section>
+      <MDXRemote source={mdxContent} components={CustomComponents} />
+      <Comments />
     </article>
   );
 }
--- src/utils/parseMdx.ts
+++ src/utils/parseMdx.ts
@@ -3,15 +3,6 @@ import path from "path";
 import matter from "gray-matter";
 import { MDX_DIRECTORY } from "@/constants/path";
 
-export type TOCSection = TOCSubSection & {
-  subSections: TOCSubSection[];
-};
-
-export type TOCSubSection = {
-  slug: string;
-  text: string;
-};
-
 export const getMdxFileDataBySlug = (slug: string) => {
   const mdxFilePath = path.join(MDX_DIRECTORY, `${slug}.mdx`);
   const mdxFileContents = fs.readFileSync(mdxFilePath, "utf-8");
@@ -29,41 +20,4 @@ export const getAllMdxMetadataAndSlug = () => {
     const { mdxMetaData } = getMdxFileDataBySlug(slug);
     return { mdxMetaData, slug };
   });
-};
-
-export const getHeadingsForTOC = (slug: string) => {
-  const mdxFilePath = path.join(MDX_DIRECTORY, `${slug}.mdx`);
-  const mdxFileContents = fs.readFileSync(mdxFilePath, "utf-8");
-  const { content: mdxContent } = matter(mdxFileContents);
-
-  return mdxContent
-    .split("\n")
-    .filter((line) => line.match(/(^#{1,3})\s/))
-    .reduce<TOCSection[]>((ac, rawHeading) => {
-      const nac = [...ac];
-      const removeMdx = rawHeading
-        .replace(/^##*\s/, "")
-        .replace(/[*,~]{2,}/g, "")
-        .replace(/(?<=\])\((.*?)\)/g, "")
-        .replace(/(?<!\S)((http)(s?):\/\/|www\.).+?(?=\s)/g, "");
-
-      const section = {
-        slug: removeMdx
-          .trim()
-          .toLowerCase()
-          .replace(/[^a-z0-9ㄱ-ㅎ|ㅏ-ㅣ|가-힣 -]/g, "")
-          .replace(/\s/g, "-"),
-        text: removeMdx,
-      };
-
-      const isSubTitle = rawHeading.split("#").length - 1 === 3;
-
-      if (ac.length && isSubTitle) {
-        nac.at(-1)?.subSections.push(section);
-      } else {
-        nac.push({ ...section, subSections: [] });
-      }
-
-      return nac;
-    }, []);
 };
