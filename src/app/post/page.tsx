0cdc77c19ccc025b1d21eebd24b2e5c005545b78
JustDevRae <ksrae165@gmail.com>
2025-03-15 22:37:27 +0900

    design: 전체 페이지 margin-top-80px 적용

--- src/app/post/page.tsx
+++ src/app/post/page.tsx
@@ -5,7 +5,7 @@ export default async function PostPage() {
   const posts = getAllMdxMetadataAndSlug();
 
   return (
-    <ul className="mt-12 grid grid-cols-1 gap-4">
+    <ul className="grid grid-cols-1 gap-4">
       {posts.map((post) => (
         <li key={post.slug}>
           <PostCard
