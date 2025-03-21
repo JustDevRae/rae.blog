f1b87347eb8d292d6f1526475a1a0466ef798c2c
JustDevRae <ksrae165@gmail.com>
2025-03-21 14:50:09 +0900

    refactor: Header 컴포넌트 코드 개선 - Link 컴포넌트 경로를 직관적으로 수정 - Header 컴포넌트가 /post/ 경로가 아닐 경우 숨김 기능 비활성화

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,31 +1,36 @@
 "use client";
 
-import { useShowAndHideHeader } from "@/hooks/useShowAndHideHeader";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import ThemeDropdown from "../ThemeDropdown";
-import Logo from "../Logo";
-import ProgressBar from "../ProgressBar";
+import { useShowAndHideHeader } from "@/hooks/useShowAndHideHeader";
+import { clsx } from "clsx";
+import Logo from "@/components/Logo";
+import ProgressBar from "@/components/ProgressBar";
+import ThemeDropdown from "@/components/ThemeDropdown";
 
 export default function Header() {
-  const { visible } = useShowAndHideHeader();
   const pathname = usePathname();
+  const isScrollAblePage = pathname.startsWith("/post/");
+  const { visible } = useShowAndHideHeader();
 
-  const isPostPage = pathname.startsWith("/post/");
+  const showHeader = isScrollAblePage ? visible : true;
 
   return (
     <header
-      className={`fixed left-1/2 top-0 flex h-[50px] w-full -translate-x-1/2 transform items-center justify-around px-[100px] font-semibold backdrop-blur-sm transition-transform duration-300 mobile:justify-between mobile:px-[30px] ${
-        visible ? "translate-y-0" : "-translate-y-full"
-      }`}
+      className={clsx(
+        "fixed left-1/2 top-0 flex h-[50px] w-full -translate-x-1/2 transform items-center justify-around px-[100px] font-semibold backdrop-blur-sm transition-transform duration-300 mobile:justify-between mobile:px-[30px]",
+        {
+          "translate-y-0": showHeader,
+          "-translate-y-full": !showHeader,
+        },
+      )}
     >
       <Logo />
       <nav className="flex items-center gap-4">
-        <Link href={{ pathname: "/post", query: { page: "1" } }}>Post</Link>
-
+        <Link href="/post?page=1">Post</Link>
         <ThemeDropdown />
       </nav>
-      {isPostPage && <ProgressBar />}
+      {isScrollAblePage && <ProgressBar />}
     </header>
   );
 }
