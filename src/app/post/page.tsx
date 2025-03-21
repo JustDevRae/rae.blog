9d0a8fab8eda68ebc054755167462f49291e4287
JustDevRae <ksrae165@gmail.com>
2025-03-21 13:40:51 +0900

    feat: Susoense 컴포넌트 사용 적용한 컴포너트 목록 - SearchBar - TagButton - PostListContainer

--- src/app/post/page.tsx
+++ src/app/post/page.tsx
@@ -3,19 +3,27 @@ import SearchBar from "@/components/SearchBar";
 import TagButton from "@/components/TagButton";
 import PostListContainer from "@/components/PostListContainer";
 
+import { Suspense } from "react";
+
 export default function PostPage() {
   const posts = getAllPostData();
   const uniqueTagArray = getAllUniqueTags();
 
   return (
     <div>
-      <SearchBar />
+      <Suspense fallback={<div>검색창 로딩 중...</div>}>
+        <SearchBar />
+      </Suspense>
       <div className="flex gap-3">
         {uniqueTagArray.map((tag) => (
-          <TagButton key={tag} tag={tag} />
+          <Suspense key={tag} fallback={<div>태그 로딩 중...</div>}>
+            <TagButton tag={tag} />
+          </Suspense>
         ))}
       </div>
-      <PostListContainer posts={posts} />
+      <Suspense fallback={<div>게시글 목록 로딩 중...</div>}>
+        <PostListContainer posts={posts} />
+      </Suspense>
     </div>
   );
 }
