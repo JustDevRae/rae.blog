218b996afb2e3b801e8d69a0a28702035df38095
JustDevRae <ksrae165@gmail.com>
2025-03-21 15:13:21 +0900

    style: Footer 디자인 변경

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,30 +1,10 @@
 export default function Footer() {
   return (
-    <footer className="bg-blue-700 py-4 text-center">
-      <div className="container mx-auto">
+    <footer className="border-grid md:py-0 border-t py-6">
+      <div className="mx-auto flex items-center justify-center gap-4">
         <p>&copy;2025. JustDevRae. All rights reserved.</p>
-        <div className="mt-2 flex justify-center gap-4">
-          <a
-            href="https://github.com/yourusername"
-            target="_blank"
-            className="hover:text-cyan-400"
-          >
-            GitHub
-          </a>
-          <a
-            href="https://linkedin.com/in/yourusername"
-            target="_blank"
-            className="hover:text-cyan-400"
-          >
-            LinkedIn
-          </a>
-          <a
-            href="mailto:your.email@example.com"
-            className="hover:text-cyan-400"
-          >
-            Email
-          </a>
-        </div>
+        <a href="https://github.com/JustDevRae">GitHub</a>
+        <a href="mailto:ksrae165@gamil.com">Email</a>
       </div>
     </footer>
   );
