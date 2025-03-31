d79d88bdd6af3cc77e9f0572adf10f5ea2f7d7b0
JustDevRae <ksrae165@gmail.com>
2025-03-31 11:48:25 +0900

    style: header nav 반응형 너비 변경

--- src/components/layout/HeaderWrapper.tsx
+++ src/components/layout/HeaderWrapper.tsx
@@ -14,9 +14,11 @@ export default function HeaderWrapper({
     <header
       className={cn(
         "fixed left-1/2 top-0 z-50 w-full -translate-x-1/2",
-        "flex h-[50px] transform items-center justify-around px-[100px]",
+        "flex h-[50px] transform items-center",
         "border-b font-semibold backdrop-blur-sm",
         "transition-transform duration-300",
+        "desktop:justify-around desktop:px-[100px]",
+        "tablet:justify-between tablet:px-[50px]",
         "mobile:justify-between mobile:px-[30px]",
         isVisible ? "translate-y-0" : "-translate-y-full",
       )}
