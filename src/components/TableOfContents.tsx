bbe618f3c6e818d9f2acc8a6e39b8b203b5f702c
JustDevRae <ksrae165@gmail.com>
2025-04-02 10:08:16 +0900

    feat: TOC 컴포넌트의 useToScroll 메서드를 커스텀 훅으로 분리

--- src/components/TableOfContents.tsx
+++ src/components/TableOfContents.tsx
@@ -1,71 +1,12 @@
 "use client";
 
-import { throttle } from "es-toolkit";
-import { useEffect, useState } from "react";
-
 import { TOCSection } from "@/types/type";
 import { cn } from "@/lib/utils";
-
-const useTocScroll = (tableOfContents: TOCSection[]) => {
-  const [currentSectionSlug, setCurrentSectionSlug] = useState<string>();
-
-  useEffect(() => {
-    if (tableOfContents.length === 0) return;
-
-    let headings: { id: string; top: number }[];
-    let pageTop = 0;
-
-    const onResize = () => {
-      headings = Array.from(document.querySelectorAll<HTMLElement>("h2")).map(
-        (element) => ({
-          id: element.id,
-          top: element.offsetTop,
-        }),
-      );
-
-      pageTop = parseFloat(
-        window
-          .getComputedStyle(document.documentElement)
-          .getPropertyValue("--page-top")
-          .match(/[\d.]+/)?.[0] ?? "0",
-      );
-    };
-
-    const onScroll = throttle(() => {
-      if (!headings) return;
-
-      let current: typeof currentSectionSlug;
-      const top = window.scrollY + pageTop;
-      const HEADING_OFFSET = 10;
-
-      headings.forEach((heading) => {
-        if (top >= heading.top - HEADING_OFFSET) {
-          current = heading.id;
-        }
-      });
-
-      setCurrentSectionSlug(current);
-    }, 100);
-
-    onResize();
-    onScroll();
-
-    window.addEventListener("scroll", onScroll, { capture: true });
-    window.addEventListener("resize", onResize, { capture: true });
-
-    return () => {
-      window.removeEventListener("scroll", onScroll, { capture: true });
-      window.removeEventListener("resize", onResize, { capture: true });
-    };
-  }, [tableOfContents]);
-
-  return { currentSectionSlug };
-};
+import { useTocScroll } from "@/hooks/useToScroll";
 
 export default function TableOfContent({
   toc,
   className,
-  ...props
 }: {
   toc: TOCSection[];
   className?: string;
@@ -73,7 +14,7 @@ export default function TableOfContent({
   const { currentSectionSlug } = useTocScroll(toc);
 
   return (
-    <div {...props} className={cn("space-y-2.5 font-sans text-sm", className)}>
+    <div className={cn("space-y-2.5 font-sans text-sm", className)}>
       <h4 className="text-base font-semibold">On This Page</h4>
       <ul>
         {toc.map((section) => (
