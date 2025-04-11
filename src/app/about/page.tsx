5ed892ec83ba9c1afb549b569ca8bb3a7cd58c81
JustDevRae <ksrae165@gmail.com>
2025-04-11 14:49:26 +0900

    style: about 페이지에 프로필 이미지 추가

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,8 +1,15 @@
+import Image from "next/image";
+
 export default function About() {
   return (
     <main>
-      <div>Hi! I am JustDevRae.</div>
-      <div>My Project</div>
+      <div className="flex items-center gap-5 mobile:flex-col">
+        <Image src="/logo.png" alt="logo" width={300} height={300} priority />
+        <div className="w-[300px] flex-wrap">
+          <p>안녕하세요. </p>
+          <p>프론트엔드 개발자 김승래입니다.</p>
+        </div>
+      </div>
     </main>
   );
 }
