2d45ee13e8e32c42bfb4e62b763c71ff5c9d03ad
JustDevRae <ksrae165@gmail.com>
2025-04-18 14:40:28 +0900

    feat: footer 컴포넌트 스타일 수정 - Github, Gmail 아이콘 컴포넌트 생성 - 외부 링크를 나타내는 태그들을 copyright 위에 나타도록 변경 - copyright font-size 및 font-weight 변경 - 모바일 반응형 제거

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,25 +1,16 @@
+import Github from "@/components/icons/github";
+import Gmail from "@/components/icons/gmai";
+
 export default function Footer() {
   return (
-    <footer className="flex items-center justify-center gap-4 border-t py-6 mobile:flex-col">
-      <p>&copy;2025. JustDevRae. All rights reserved.</p>
-      <div className="flex items-center justify-center gap-2">
-        <a
-          href="https://github.com/JustDevRae"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="underline"
-        >
-          GitHub
-        </a>
-        <a
-          href="mailto:ksrae165@gamil.com"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="underline"
-        >
-          Email
-        </a>
+    <footer className="flex flex-col items-center justify-center gap-3 border-t py-6">
+      <div className="flex items-center justify-center gap-3">
+        <Github />
+        <Gmail />
       </div>
+      <p className="text-sm font-semibold">
+        &copy;2025. JustDevRae. All rights reserved.
+      </p>
     </footer>
   );
 }
