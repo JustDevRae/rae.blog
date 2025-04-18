ff06cd2d1f1a312c581cf035fb00d5c6a30e3d23
JustDevRae <ksrae165@gmail.com>
2025-04-18 12:40:48 +0900

    feat: footer 컴포넌트 외부 링크에 target, rel 속성 추가 및 underline 스타일 적용

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -3,8 +3,22 @@ export default function Footer() {
     <footer className="flex items-center justify-center gap-4 border-t py-6 mobile:flex-col">
       <p>&copy;2025. JustDevRae. All rights reserved.</p>
       <div className="flex items-center justify-center gap-2">
-        <a href="https://github.com/JustDevRae">GitHub</a>
-        <a href="mailto:ksrae165@gamil.com">Email</a>
+        <a
+          href="https://github.com/JustDevRae"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="underline"
+        >
+          GitHub
+        </a>
+        <a
+          href="mailto:ksrae165@gamil.com"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="underline"
+        >
+          Email
+        </a>
       </div>
     </footer>
   );
