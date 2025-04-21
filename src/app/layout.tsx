ae4729f872e13e8ba2334a519c3c5bdc5cf232eb
JustDevRae <ksrae165@gmail.com>
2025-04-21 11:58:43 +0900

    style: layout.tsx의 main 태그 최소/최대 너비 설정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -58,7 +58,9 @@ export default function RootLayout({
           disableTransitionOnChange
         >
           <Header />
-          <main className="mx-auto mb-12 mt-20 flex-grow px-4">{children}</main>
+          <main className="mx-auto mb-12 mt-20 w-full min-w-[340px] max-w-[700px] flex-grow px-4">
+            {children}
+          </main>
           <Footer />
         </ThemeProvider>
       </body>
