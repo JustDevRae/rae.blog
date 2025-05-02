a6cceb06826f6680b30aa5958535c15080aaf124
JustDevRae <ksrae165@gmail.com>
2025-05-02 09:16:38 +0900

    chore: baseURL 수정

--- src/constants/path.ts
+++ src/constants/path.ts
@@ -2,4 +2,4 @@ import path from "path";
 
 export const MDX_DIRECTORY = path.join(process.cwd(), "src/contents");
 
-export const baseURL = "https://rae-blog.vercel.app";
+export const baseURL = "https://www.dev-rae.blog";
