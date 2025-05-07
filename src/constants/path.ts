5a166a3f3685354a40846efedcdcd2ae2ff96ee3
JustDevRae <ksrae165@gmail.com>
2025-05-07 18:44:46 +0900

    chore: baseURL 경로 수정

--- src/constants/path.ts
+++ src/constants/path.ts
@@ -2,4 +2,4 @@ import path from "path";
 
 export const MDX_DIRECTORY = path.join(process.cwd(), "src/contents");
 
-export const baseURL = "https://www.dev-rae.blog";
+export const baseURL = "https://dev-rae.blog";
