b137546e4a748fb074c5a7c8484743005bb3eebc
JustDevRae <ksrae165@gmail.com>
2025-05-15 21:33:23 +0900

    feat: 게시글 목록 페이지 metadata 추가

--- src/app/(main)/post/page.tsx
+++ src/app/(main)/post/page.tsx
@@ -3,6 +3,13 @@ import SearchBar from "@/components/search-bar";
 import TagList from "@/components/tag-list";
 import PostListContainer from "@/components/post-list-container";
 import { Suspense } from "react";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Rae | 게시글 목록",
+  description:
+    "프론트엔드 개발에 관해서 제가 공부하고 정리한 게시글을 확인할 수 있습니다.",
+};
 
 export default function PostPage() {
   const postArray = getAllPostMeta();
