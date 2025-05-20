b15d22eac00d131d1522e69163c4d7982a992fad
JustDevRae <ksrae165@gmail.com>
2025-05-20 21:55:09 +0900

    feat: MDXLink 컴포넌트 구현

--- src/components/mdx-custom-components.tsx
+++ src/components/mdx-custom-components.tsx
@@ -6,4 +6,16 @@ export const components = {
       <Image src={src} alt={alt} width={1000} height={600} className="m-0" />
     );
   },
+  MDXLink: ({ href, label }: { href: string; label: string }) => {
+    return (
+      <a
+        href={href}
+        target="_blank"
+        rel="noopener noreferrer"
+        className="inline underline"
+      >
+        {label}
+      </a>
+    );
+  },
 };
