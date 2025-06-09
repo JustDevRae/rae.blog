93da2b5c32ff40b0b578e794494611f89dfb45e9
JustDevRae <ksrae165@gmail.com>
2025-06-09 17:10:10 +0900

    feat: toc 구조 개선 및 h3 서브섹션 포함되도록 파서 로직 확장

--- src/lib/toc.ts
+++ src/lib/toc.ts
@@ -1,21 +1,41 @@
+export type TOCSubSection = {
+  slug: string;
+  text: string;
+};
+
+export type TOCSection = TOCSubSection & {
+  subSections: TOCSubSection[];
+};
+
 export const parseToc = (source: string) => {
   return source
     .split("\n")
-    .filter((line) => line.match(/^##\s/))
-    .map((rawHeading) => {
-      const removeMdx = rawHeading
-        .replace(/^##\s/, "")
+    .filter((line) => line.match(/(^#{1,3})\s/))
+    .reduce<TOCSection[]>((ac, markdownHeadingText) => {
+      const newAccumulator = [...ac];
+      const pureHeadingText = markdownHeadingText
+        .replace(/^##*\s/, "")
         .replace(/[*,~]{2,}/g, "")
         .replace(/(?<=\])\((.*?)\)/g, "")
         .replace(/(?<!\S)((http)(s?):\/\/|www\.).+?(?=\s)/g, "");
 
-      return {
-        slug: removeMdx
+      const section = {
+        slug: pureHeadingText
           .trim()
           .toLowerCase()
           .replace(/[^a-z0-9ㄱ-ㅎ|ㅏ-ㅣ|가-힣 -]/g, "")
           .replace(/\s/g, "-"),
-        text: removeMdx,
+        text: pureHeadingText,
       };
-    });
+
+      const isSubTitle = markdownHeadingText.split("#").length - 1 === 3;
+
+      if (ac.length && isSubTitle) {
+        newAccumulator.at(-1)?.subSections.push(section);
+      } else {
+        newAccumulator.push({ ...section, subSections: [] });
+      }
+
+      return newAccumulator;
+    }, []);
 };
