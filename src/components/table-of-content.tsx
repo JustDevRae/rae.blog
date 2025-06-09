5ee3c558c4035e2decd6fd56d279432fab5f51b0
JustDevRae <ksrae165@gmail.com>
2025-06-10 00:30:11 +0900

    feat: useToScroll에서 h3 헤딩까지 감지하여 toc 텍스트 활성화하도록 수정

--- src/components/table-of-content.tsx
+++ src/components/table-of-content.tsx
@@ -1,33 +1,25 @@
 "use client";
 
+import { useTocScroll } from "@/hooks/useToScroll";
 import { TOCSection } from "@/lib/toc";
 import { cn } from "@/lib/utils";
 
 export default function TableOfContent({ toc }: { toc: TOCSection[] }) {
+  const { currentSectionSlug } = useTocScroll(toc);
   return (
     <>
       {/* top-table-of-content */}
       <aside className="my-10 block border-b-2 desktop:hidden">
         <h4 className="text-2xl font-semibold">On This Page</h4>
-        <ul className="ml-10 space-y-3 py-10">
+        <ul className="ml-10 space-y-2 py-10 text-base font-medium text-gray-700 no-underline dark:text-zinc-400">
           {toc.map((section) => (
             <li key={section.text}>
-              <a
-                className="text-base font-medium text-zinc-400 no-underline"
-                href={`#${section.slug}`}
-              >
-                {section.text}
-              </a>
+              <a href={`#${section.slug}`}>{section.text}</a>
               {section.subSections.length > 0 && (
-                <ul className="ml-6 mt-3 space-y-2">
+                <ul className="ml-4 mt-1 space-y-2">
                   {section.subSections.map((sub) => (
                     <li key={sub.text}>
-                      <a
-                        className="text-sm text-zinc-400 no-underline"
-                        href={`#${sub.slug}`}
-                      >
-                        {sub.text}
-                      </a>
+                      <a href={`#${sub.slug}`}>{sub.text}</a>
                     </li>
                   ))}
                 </ul>
@@ -42,21 +34,33 @@ export default function TableOfContent({ toc }: { toc: TOCSection[] }) {
         <div
           className={cn(
             "space-y-2.5 font-sans text-sm",
-            "sticky top-[200px] z-10 ml-[50%] mt-[200px] w-[200px]",
+            "sticky top-[200px] z-10 mt-[200px] w-[200px]",
           )}
         >
           <h4 className="text-lg font-semibold">On This Page</h4>
-          <ul className="pl-4">
+          <ul className="space-y-2 pl-4 text-base font-medium text-gray-700 no-underline dark:text-zinc-400">
             {toc.map((section) => (
-              <li key={section.text} className="mb-1">
-                <a className="text-zinc-400" href={`#${section.slug}`}>
+              <li key={section.text}>
+                <a
+                  href={`#${section.slug}`}
+                  className={cn(
+                    currentSectionSlug === section.slug &&
+                      "font-semibold text-black dark:text-white",
+                  )}
+                >
                   {section.text}
                 </a>
                 {section.subSections.length > 0 && (
-                  <ul className="ml-4 mt-1 space-y-1">
+                  <ul className="ml-4 mt-1 space-y-2 text-base font-medium text-gray-700 no-underline dark:text-zinc-400">
                     {section.subSections.map((sub) => (
                       <li key={sub.text}>
-                        <a className="text-zinc-400" href={`#${sub.slug}`}>
+                        <a
+                          href={`#${sub.slug}`}
+                          className={cn(
+                            currentSectionSlug === sub.slug &&
+                              "font-semibold text-black dark:text-white",
+                          )}
+                        >
                           {sub.text}
                         </a>
                       </li>
