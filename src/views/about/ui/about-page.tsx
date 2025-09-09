c7158055f0a14c8e9c29756dc98df25ecde143d2
JustDevRae <ksrae165@gmail.com>
2025-09-09 19:11:19 +0900

    chore: ./app/about/page.tsx에서 src/views/about의 페이지 컴포넌트를 렌더링하도록 수정

---
+++ src/views/about/ui/about-page.tsx
@@ -0,0 +1,21 @@
+import Image from "next/image";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "About",
+  description: "개발자 김승래에 대해 소개합니다.",
+};
+
+export default function About() {
+  return (
+    <main>
+      <div className="flex items-center gap-5 mobile:flex-col">
+        <Image src="/logo.png" alt="logo" width={300} height={300} priority />
+        <div className="w-[300px] flex-wrap">
+          <p>안녕하세요. </p>
+          <p>프론트엔드 개발자 김승래입니다.</p>
+        </div>
+      </div>
+    </main>
+  );
+}
