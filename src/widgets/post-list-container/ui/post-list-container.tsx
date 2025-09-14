111a445d403ef278389b46e811f32ddb415a8b2a
JustDevRae <ksrae165@gmail.com>
2025-09-14 22:10:28 +0900

    refactor: post-list widget 컴포넌트를 post-list-container widget 컴포넌트에 병합

--- src/widgets/post-list-container/ui/post-list-container.tsx
+++ src/widgets/post-list-container/ui/post-list-container.tsx
@@ -3,7 +3,8 @@
 import { useSearchParams } from "next/navigation";
 import PostListPagination from "@/features/paginate-posts/ui/post-list-pagination";
 import { Post } from "@/entities/post/model/type";
-import { PostList } from "@/widgets/post-list/ui/post-list";
+import { PostCard } from "@/entities/post/ui/post-card";
+import { AlertCircle } from "lucide-react";
 
 interface PostListContainerProps {
   postArray: Post[];
@@ -38,7 +39,28 @@ export function PostListContainer({ postArray }: PostListContainerProps) {
 
   return (
     <div>
-      <PostList posts={paginatedPosts} />
+      <div className="mt-6">
+        <ul className="grid grid-cols-1 gap-4">
+          {paginatedPosts.length > 0 ? (
+            paginatedPosts.map((post) => (
+              <li key={post.slug}>
+                <PostCard
+                  slug={post.slug}
+                  title={post.postMetaData.title}
+                  description={post.postMetaData.description}
+                  tags={post.postMetaData.tags}
+                  date={post.postMetaData.date}
+                />
+              </li>
+            ))
+          ) : (
+            <div className="flex flex-col items-center justify-center gap-4 py-20 text-zinc-600">
+              <AlertCircle className="h-10 w-10" />
+              <p className="text-xl">게시글을 찾을 수 없습니다.</p>
+            </div>
+          )}
+        </ul>
+      </div>
       {totalPages > 0 && <PostListPagination totalPages={totalPages} />}
     </div>
   );
