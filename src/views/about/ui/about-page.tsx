f1eb23091a357d0a57e494f75f41fb3140c58f31
JustDevRae <ksrae165@gmail.com>
2025-09-15 12:52:50 +0900

    refactor: about-page.tsx mobile-first 적용

--- src/views/about/ui/about-page.tsx
+++ src/views/about/ui/about-page.tsx
@@ -9,7 +9,7 @@ export const metadata: Metadata = {
 export default function AboutPage() {
   return (
     <main>
-      <div className="flex items-center gap-5 mobile:flex-col">
+      <div className="flex flex-col items-center gap-5 tablet:flex-row">
         <Image src="/logo.png" alt="logo" width={300} height={300} priority />
         <div className="w-[300px] flex-wrap">
           <p>안녕하세요. </p>
